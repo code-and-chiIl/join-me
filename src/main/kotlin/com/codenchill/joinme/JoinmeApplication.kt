6d62eceabacc71f0d65e867eaf5e6682fac8e5a9
yayz <coffeeminus92@gmail.com>
2022-10-17 20:10:51 +0900

    join me initial commit

---
+++ src/main/kotlin/com/codenchill/joinme/JoinmeApplication.kt
@@ -0,0 +1,11 @@
+package com.codenchill.joinme
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JoinmeApplication
+
+fun main(args: Array<String>) {
+    runApplication<JoinmeApplication>(*args)
+}
